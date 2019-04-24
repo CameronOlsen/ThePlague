f86f6e7464c52a503dcdf13fe67a6b5d9f180f5a
CameronOlsen <49822773+CameronOlsen@users.noreply.github.com>
2019-04-23 20:37:13 -0500

    Discretizing the Domain

---
+++ src/Project.m
@@ -0,0 +1,9 @@
+% Domain: -pi<X<pi   -pi<y<pi
+% Starting by Discretizing FEB 5th PAGE 2 BOT
+% L: number of points, h: interval, N: number of points
+Lx = 2*pi;
+Ly = 2*pi;
+Nx = 5000;
+Ny = 5000;
+hx = Lx/(Nx+1)
+hy = Ly/(Ny+1)
